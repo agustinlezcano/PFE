52a40b76728b2b44efd7db63f03de805cf800c3c
agustinlezcano <50871051+agustinlezcano@users.noreply.github.com>
2023-08-05 22:06:34 -0300

    Update modelo.m

--- robot_MK2_matlab/modelo.m
+++ robot_MK2_matlab/modelo.m
@@ -1,144 +1,164 @@
-function [q_obj, flag, qqs] = cInversa(q_ant, dh, T, R)
+clc;clear;
+%% Modelo del robot
+%Matriz DENAVIT-HARTEMBERG
+%Parametros
 
-%% Cinematica Inversa
-L1 =1.678;
-L2 =3.3345;
-L3 =3.6369;
+%------------------Robot 1--------------------
+%---------------------------------------------
+%corregir las longiudes
+L1 =0.057;
+L2 =0.135;
+L3 =0.147;
+L1 =1;
+L2 =1;
+L3 =1;
+%en el original es pi/2
+dh = [0 L1  0 -pi/2   0;
+      0 0   L2  0     0;
+      0 0   L3  0     0];
 
-%[q_obj, flag, qqs] = cinv(q_ini, dh, T, R)
-T = invHomog(R.base.double) * T * invHomog(R.tool.double);
-Pm = T(1:3,4) %posicion final deseada
-%4 soluciones
-qq=zeros(3,4);
+R=SerialLink(dh,'name','Brazo Principal MK2');
+%LIMITES ARTICULARES
+R.qlim(1,1:2) = [-185,  185]*pi/180;
+R.qlim(2,1:2) = [-185,  185]*pi/180;
+R.qlim(3,1:2) = [-185, 228]*pi/180;
 
-%q1---------------------------------------------------------
+% R.offset = [pi -pi/2 -pi/2];
+R.offset = [pi -pi/2 -pi/2];
 
-q1(1) = atan2(Pm(2), Pm(1));
+%cambiar las dimensiones de tool
+R.tool = transl(.0817, 0, 0) * trotz(-pi/2) * transl(0.0419, 0, 0);
+R.tool = eye(4)
+Tool = R.tool.double;
+R.base = transl(0, 0, 0.0);
+Base = R.base.double;
+%------------------Robot 2--------------------
+%---------------------------------------------
+L1a = 0.0694;
+L2a = 0.057;
+L3a = 0.135;
+L4a = 0.2043;
+Xa = 0.025;
+dh = [0 L1a  0 pi/2   0;
+      0 Xa   L2a  0     0;
+     -pi/2 0   L3a  0 0;
+     -pi/2 -Xa L4a 0 0];
 
-if q1(1) > 0
-    q1(2) = q1(1) - pi; 
-else
-    q1(2) = q1(1) + pi; 
-end
+R2=SerialLink(dh,'name','Brazo Secundario MK2');
+%LIMITES ARTICULARES
+R2.qlim(1,1:2) = [-185,  185]*pi/180;
+R2.qlim(2,1:2) = [-185,  185]*pi/180;
+R2.qlim(3,1:2) = [-228, 228]*pi/180;
+R2.qlim(4,1:2) = [-185, 228]*pi/180;
 
-qq(1,1:2) = [1 1] * q1(1);
-qq(1,3:4) = [1 1] * q1(2);
+R2.offset = [0 pi -pi/2 -pi/2];
+R2.tool = transl(.0817, 0, 0) * trotz(-pi/2) * transl(0.0419, 0, 0);
+Tool2 = R2.tool.double;
+R2.base = transl(0, 0, 0.0);
+Base2 = R2.base.double;
+%------------------Robot 3--------------------
+%---------------------------------------------
+L1c =0.057;
+L2c =0.038;
+L3c =0.155;
+L4c =0.0685;
+L5c =0.1468;
+L6c =0.04345;
+dh = [0 L1c  0 pi/2   0;
+     0 0.0205   L2c  0     0;
+      pi/2 -0.0075   L3c  0     0;
+      0 0 L4c 0 0;
+      0 -0.0125 L5c 0 0;
+      .8406 0 L6c 0 0];
 
-% q2---------------------------------------------------------
+R3=SerialLink(dh, 'name','Soporte MK2');
+%LIMITES ARTICULARES
+R3.qlim(1,1:2) = [-185,  185]*pi/180;
+R3.qlim(2,1:2) = [-195,  195]*pi/180;
+R3.qlim(3,1:2) = [-85, 228]*pi/180;
 
+R3.offset = [0 pi -pi/2 -pi/2 0 0];
+R3.tool = transl(.0817, 0, 0) * trotz(-pi/2) * transl(0.0419, 0, 0);
+Tool3 = R3.tool.double;
+R3.base = transl(0, 0, 0.0);
+Base3 = R3.base.double;
 
-% T1 = A_dh(dh(1,:), q1(1));
-% p21 = invHomog(T1) * [Pm; 1]
-% B = atan2(p21(2), p21(1));
-% r = sqrt(p21(1)^2 + p21(2)^2);
-% G = (acos((L2^2 + r^2 - L3^2) / (2 * r * L2)));
-% 
-% q2(1) = B - real(G);
-% q2(2) = B + real(G);
-% q3(1) = (acos((r^2 - L2^2 - L3^2)/ (2 * L2 * L3)));
-% q3(2) = (acos((r^2 - L2^2 - L3^2)/ (2 * L2 * L3)));
-% img1=~isreal(G);
-% 
-% T1 = A_dh(dh(1,:), q1(2));
-% p22 = invHomog(T1) * [Pm; 1];
-% B = atan2(p22(2), p22(1));
-% r = sqrt(p22(1)^2 + p22(2)^2);
-% G = (acos((L2^2 + r^2 - L3^2) / (2 * r * L2)));
-% 
-% q2(3) = B - real(G);
-% q2(4) = B + real(G);
-% q3(3) = (acos((r^2 - L2^2 - L3^2)/ (2 * L2 * L3)));
-% q3(4) = (acos((r^2 - L2^2 - L3^2)/ (2 * L2 * L3)));
-% img2=~isreal(G);
-% 
-% qq(2,:) = [q2(1) q2(2) q2(3) q2(4)];
-% qq(3,:) = [q3(1) q3(2) q3(3) q3(4)];
-% %sacar offset
-% qq = qq - R.offset' * ones(1,4);
+%% ANGULOS INICIALES
+q_ini=[0 0 0];
 
-%% Prueba: 4/8 cambio en q2
-T1 = A_dh(dh(1,:), q1(1));
-p21 = invHomog(T1) * [Pm; 1]
-B = atan2(p21(2), p21(1));   %o es p21(2)y 3 en  vez de 1 2?
-d= sqrt(p21(1)^2 + p21(2)^2);
-C = (acos((L2^2 + d^2 - L3^2) / (2 * d * L2)));
-q2(1)=B - real(C);
-q2(2)=B + real(C);
-img1=~isreal(C);
+%% Menu principal 
+fprintf('Bienvenido a nuestro Proecto Final de Estudios\n')
+fprintf('Realizado sobre brazo MK2 de 3 gdl\n\n')
+fprintf('Las opciones son las siguientes:\n\n')
+fprintf('1:Espacio de Trabajo \n')
+fprintf('2: Cinematica inversa \n')
+fprintf('3: Singularidades \n')
+fprintf('4: Trayectoria \n\n')
+fprintf('5: Robot completo \n\n')
+x=input('Ingrese opcion: ');
 
-T1 = A_dh(dh(1,:), q1(2));
-p22 = invHomog(T1) * [Pm; 1]
-B = atan2(p22(2), p22(1));   %o es p21(2)y 3 en  vez de 1 2?
-d= sqrt(p22(1)^2 + p22(2)^2);
-C = (acos((L2^2 + d^2 - L3^2) / (2 * d * L2)));
-q2(3)=B - real(C);
-q2(4)=B + real(C);
-img2=~isreal(C);
+switch x
+    case 1
+        figure()
+        R.plot(q_ini,'trail', {'r'})
+        hold on
+        R.teach()
+    case 2
+        % Cinematica Inversa
+        T = transl(0.43,-0.993,1.958);  %Posicion objetivo. z = 0.317
+        T = Base * T * Tool;
+        [q_obj, flag, qqs] = cInversa(q_ini, dh, T, R)
+        
+        % Comparacion con Ikine 
+        L1 =0.057;
+        L2 =0.135;
+        L3 =0.147;
+        L1 =1;
+        L2 =1;
+        L3 =1;
+        dh = [0 L1  0 -pi/2   0;
+              0 0   L2  0     0;
+              0 0   L3  0     0];
 
-qq(2,:) = [q2(1) q2(2) q2(3) q2(4)];
-%--------------q3----------------------------------
+        R1=SerialLink(dh,'name','MK2-b');
+        %LIMITES ARTICULARES
+        R1.qlim(1,1:2) = [-185,  185]*pi/180;
+        R1.qlim(2,1:2) = [-185,  185]*pi/180;
+        R1.qlim(3,1:2) = [-185, 228]*pi/180;
 
-for i=1:4
-    T1 = A_dh(dh(1,:), qq(1,i));
-    p22 = invHomog(T1) * [Pm; 1];
-    d= (p22(1)^2 + p22(2)^2);
-    q3(i) = pi-(acos((d^2 - L2^2 - L3^2) / -(2 * L3 * L2)));
-end
+        R1.offset = [pi -pi/2 -pi/2];
+        R1.tool = transl(.0817, 0, 0) * trotz(-pi/2) * transl(0.0419, 0, 0)
+        R.tool = eye(4)
+        Tool = R1.tool.double;
+        
+        R1.base = transl(0, 0, 0.0);
+        Base = R1.base.double;
+        T1 = transl(0.43,-0.993,1.958); %Posicion objetivo
+        T1 = Base * T1 * Tool;
 
-qq(3,:) = [q3(1) q3(2) q3(3) q3(4)];
-%% menores distancia, primero las opciones reales.
-qdis=zeros(1,4);
-num=[1,2,3,4];
-qqimg=zeros(1,4);
-
-if (img1==1)
-    qqimg(1:2)=1;
-end
-
-if (img2==1)
-    qqimg(3:4)=1;
-end
-
-for i=1:4
-qdis(i)=sum(abs(qq(:,i)'-q_ant));
-end
-
-qposibles=[qdis;qqimg;num];%primer fila distancia, segunda fila 1 si es img 0 si es real, tercer fila numero de solucion.
-
-%ordeno columnas en funcion primer fila para que quede de menor a mayor distancia
-%luego ordeno en funcion de segunda fila para dejar valores imaginarios al final 
-[~,data]=sort(qposibles(1,:));
-qposibles=qposibles(:,data);
-
-[~,data]=sort(qposibles(2,:));
-qposibles=qposibles(:,data);
-qqs=[];
-for i=1:4
-qqs=[qqs qq(:,qposibles(3,i))];
-end
-indice=qposibles(3,1);
-
-q_obj=qq(:,indice)';
-
-%% revisa si la respuesta es posible o no en funcion de si es imaginaria
-fl=qposibles(2,1);
-if (fl==1)
-    flag=0; 
-    fprintf('La posicion final  esta fuera del espacio de trabajo\n se ha dado la respuesta mas cercana\n\n')
-else
-    flag=1;
-    fprintf('La posicion esta dentro del espacio de trabajo\n\n')
-end
-
-%% funciones auxiliares
-function T= A_dh(dh,q)
-T=trotz(q)*transl(dh(3),0,dh(2))*trotx(dh(4));
-end
-
-function iT = invHomog(T)
-iT = eye(4);
-iT(1:3, 1:3) = T(1:3, 1:3)';
-iT(1:3, 4) = - iT(1:3, 1:3) * T(1:3, 4);
-end
-
+        Q = R1.ikine(T1, 'mask', [1 1 1 0 0 0])
+        figure()
+%         R.plot(q_obj)
+        R.plot(qqs(:,1)')
+        hold on
+        R1.plot(Q)
+        
+        %comparacion de 4 posiciones
+%         figure()
+%         R.plot(qqs(:,1)')
+%         for i=1:4
+%             R.plot(qqs(:,i)')
+%             hold on
+%         end
+    case 5
+        q_a=[0 0 0];
+        figure()
+        R.plot(q_a,'scale', 0.6);
+        hold on
+        q_b=[0 0 0 0];
+        R2.plot(q_b,'scale', 0.6); %4 parametros
+        hold on
+        q_c=[0 0 0 0 0 0];
+        R3.plot(q_c,'scale', 0.6); %6 parametros
+        
 end
